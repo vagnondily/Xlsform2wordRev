37c26ed71c5bf2e0d3120fa356a99d0295eae848
vagnondily <77827595+vagnondily@users.noreply.github.com>
2025-11-19 15:05:46 +0300

    Update xlsform2wordRev.R

--- R/xlsform2wordRev.R
+++ R/xlsform2wordRev.R
@@ -232,7 +232,7 @@ render_question <- function(doc, row, number, label_col_name, hint_col_name, cho
 # Fonction principale 
 # ---------------------------------------------------------------------
 
-xlsform_to_wordRev <- function(xlsx = XLSFORM_PATH, output_dir = OUTPUT_DIR, template_docx = TEMPLATE_DOCX, logo_path = LOGO_PATH, doc_title = NULL) {
+xlsform_to_wordRev <- function(xlsx = XLSFORM_PATH, output_dir = NULL, template_docx = TEMPLATE_DOCX, logo_path = LOGO_PATH, doc_title = NULL) {
   message(glue("--- Démarrage du processus de génération Word ---"))
 # Sélection fichier si non fourni
 if (is.null(output_dir)) {
@@ -357,6 +357,7 @@ if (is.null(output_dir)) {
   message(glue("✅ Document généré : {final_path_display}"))
   invisible(out_docx)
 }
+
 
 
 
